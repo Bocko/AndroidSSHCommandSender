df889152401da81dd0400d05a8380f0ddcf33ff0
Botond Laszlo <lgbotond@gmail.com>
2023-11-02 16:46:58 +0100

    created a test setup for the status message

--- app/src/main/java/com/lgbotond/androidsshcommandsender/MainActivity.kt
+++ app/src/main/java/com/lgbotond/androidsshcommandsender/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.lgbotond.androidsshcommandsender
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.lgbotond.androidsshcommandsender.Xtensions.fadeOut
+import com.lgbotond.androidsshcommandsender.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    var test = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSend.setOnClickListener { sendEvent() }
+    }
+
+    private fun sendEvent() {
+        if(test) {
+            binding.tvStatus.setTextColor(getColor(R.color.error))
+            binding.tvStatus.setText("FAILED")
+        } else {
+            binding.tvStatus.setTextColor(getColor(R.color.success))
+            binding.tvStatus.setText("SUCCESS")
+        }
+
+        binding.tvStatus.fadeOut()
+
+        test = !test
     }
 }
---
+++ app/src/main/java/com/lgbotond/androidsshcommandsender/Xtensions/Xtensions.kt
@@ -0,0 +1,20 @@
+package com.lgbotond.androidsshcommandsender.Xtensions
+
+import android.view.View
+import android.view.animation.AlphaAnimation
+
+fun View.fadeIn(durationMillis: Long = 250, startOffsetMillis: Long = 1000) {
+    this.startAnimation(AlphaAnimation(0f, 1f).apply {
+        startOffset = startOffsetMillis
+        duration = durationMillis
+        fillAfter = true
+    })
+}
+
+fun View.fadeOut(durationMillis: Long = 250, startOffsetMillis: Long = 1000) {
+    this.startAnimation(AlphaAnimation(1f, 0f).apply {
+        startOffset = startOffsetMillis
+        duration = durationMillis
+        fillAfter = true
+    })
+}
